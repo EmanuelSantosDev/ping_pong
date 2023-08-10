10481a9ee7a1606359e4984b24be91d8f101573a
Emanuel Olivio dos Santos <emanuel.santos.developer@gmail.com>
2023-08-10 07:38:53 -0300

    Desenhando a linha central

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const elementoCanvas = document.querySelector('canvas')
 const contextoCanvas = elementoCanvas.getContext('2d')
+const espessuraDaLinha = 15
 
 function configuracoes() {
   elementoCanvas.width = contextoCanvas.width = window.innerWidth
@@ -7,8 +8,17 @@ function configuracoes() {
 }
 
 function desenhar() {
+  // desenhando o campo
   contextoCanvas.fillStyle = '#286047'
   contextoCanvas.fillRect(0, 0, window.innerWidth, window.innerHeight)
+
+  // desenhando a linha central
+  const x = (window.innerWidth / 2) - (espessuraDaLinha / 2)
+  const y = 0
+  const largura = espessuraDaLinha
+  const altura = window.innerHeight
+  contextoCanvas.fillStyle = '#ffffff'
+  contextoCanvas.fillRect(x, y, largura, altura)
 }
 
 configuracoes()
