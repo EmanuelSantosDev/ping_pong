d8f8827aecc48dc511e978a03a6fa8052414523c
Emanuel Santos <emanuel.santos.developer@gmail.com>
2023-08-11 08:57:56 -0300

    Criando os objetos

--- script.js
+++ script.js
@@ -1,56 +1,93 @@
 const elementoCanvas = document.querySelector('canvas')
-const contextoCanvas = elementoCanvas.getContext('2d')
+const contexto = elementoCanvas.getContext('2d')
 const espessuraDaLinha = 15
+const gapX = 10
 
+// objeto CAMPO
 const campo = {
    w: window.innerWidth,
    h: window.innerHeight,
    desenhar: function () {
-      contextoCanvas.fillStyle = '#286047'
-      contextoCanvas.fillRect(0, 0, window.innerWidth, window.innerHeight)
+      contexto.fillStyle = '#286047'
+      contexto.fillRect(0, 0, this.w, this.h)
+   }
+}
+
+// objeto LINHA
+const linha = {
+   w: 15,
+   h: campo.h,
+   desenhar: function () {
+      contexto.fillStyle = '#ffffff'
+      contexto.fillRect(campo.w / 2 - this.w / 2, 0, this.w, this.h
+      )
+   }
+}
+
+// objeto RAQUETE ESQUERDA
+const raqueteEsquerda = {
+   x: gapX,
+   y: 100,
+   w: linha.w,
+   h: 200,
+   desenhar: function () {
+      contexto.fillStyle = '#ffffff'
+      contexto.fillRect(this.x, this.y, this.w, this.h)
+   }
+}
+
+// objeto RAQUETE DIREITA
+const raqueteDireita = {
+   x: campo.w - linha.w - gapX,
+   y: 100,
+   w: linha.w,
+   h: 200,
+   desenhar: function () {
+      contexto.fillStyle = '#ffffff'
+      contexto.fillRect(this.x, this.y, this.w, this.h)
+   }
+}
+
+// objeto PLACAR
+const placar = {
+   humano: 1,
+   computador: 2,
+   desenhar: function () {
+      contexto.fillStyle = '#ffffff'
+      contexto.font = 'bold 72px Arial'
+      contexto.textAlign = 'center'
+      contexto.textBaseline = 'top'
+      contexto.fillStyle = '#01341D'
+      contexto.fillText(this.humano, campo.w / 4, 50)
+      contexto.fillText(this.computador, campo.w / 4 + campo.w / 2, 50)
+   }
+}
+
+// objeto BOLA
+const bola = {
+   x: 300,
+   y: 200,
+   r: 20,
+   desenhar: function () {
+      contexto.fillStyle = '#ffffff'
+      contexto.beginPath()
+      contexto.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
+      contexto.fill()
    }
 }
 
 function configuracoes() {
-   elementoCanvas.width = contextoCanvas.width = campo.w
-   elementoCanvas.height = contextoCanvas.height = campo.h
+   elementoCanvas.width = contexto.width = campo.w
+   elementoCanvas.height = contexto.height = campo.h
 }
 
 function desenhar() {
    campo.desenhar()
-
-   // desenhando a linha central
-   contextoCanvas.fillStyle = '#ffffff'
-   contextoCanvas.fillRect(
-      window.innerWidth / 2 - espessuraDaLinha / 2,
-      0,
-      espessuraDaLinha,
-      window.innerHeight
-   )
-
-   // desenhando a raquete esquerda
-   contextoCanvas.fillRect(10, 100, espessuraDaLinha, 200)
-
-   // desenhando a raquete direita
-   contextoCanvas.fillRect(
-      window.innerWidth - espessuraDaLinha - 10,
-      300,
-      espessuraDaLinha,
-      200
-   )
-
-   // desenhando a bola
-   contextoCanvas.beginPath()
-   contextoCanvas.arc(300, 300, 20, 0, 2 * Math.PI, false)
-   contextoCanvas.fill()
-
-   // desenhando o placar
-   contextoCanvas.font = 'bold 72px Arial'
-   contextoCanvas.textAlign = 'center'
-   contextoCanvas.textBaseline = 'top'
-   contextoCanvas.fillStyle = '#01341D'
-   contextoCanvas.fillText('0', window.innerWidth / 4, 50)
-   contextoCanvas.fillText('3', window.innerWidth / 4 + window.innerWidth / 2, 50)
+   linha.desenhar()
+   raqueteEsquerda.desenhar()
+   raqueteDireita.desenhar()
+   placar.desenhar()
+   bola.desenhar()
 }
 
 configuracoes()
